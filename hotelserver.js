122a135fc141d6d6ca754082d8fe3f9e4bea36be
JongIn <ekejaqutmqk@gmail.com>
2020-11-26 17:04:11 +0900

    호텔 서버
    최초 커밋

---
+++ hotelserver.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var http = require('http'); 
+var mysql = require('mysql');
+var bodyParser = require('body-parser');
+const send = require('send');
+
+//서버 정보
+var host = '192.168.0.4';//jonginfi.iptime.org
+var port = 5000;
+
+//DB정보
+var conn = mysql.createConnection({
+    host:'hotel.cgapmdvczori.ap-northeast-2.rds.amazonaws.com',
+    user:'admin',
+    password:'hoteladmin',
+    database:'hotel'
+})
+
+
+//서버 객체 설정
+var app = express();
+app.set('port', process.env.PROT || port);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
+
+
+//요청 및 응답
+app.get('/', (req, res) => {
+    res.send('Root');
+});
+
+app.get('/room/status', function (req, res) {
+    var sql = 'SELECT * FROM room';
+    conn.query(sql, function (err, rows, fields) {
+        if(err) {
+            res.send(err.message);
+            console.log('query is not excuted. select fail...\n' + err);
+        }
+        else{
+            res.send(rows);
+            console.log(rows);
+        }
+    });
+});
+
+/*
+app.post('/room/add', function (req, res) {
+    var body = req.body;
+    console.log(body);
+
+    var sql = 'INSERT INTO BOARD VALUES(?, ?, ?, NOW())';
+    var params = [body.id, body.title, body.content];
+    console.log(sql);
+    conn.query(sql, params, function(err) {
+        if(err) console.log('query is not excuted. insert fail...\n' + err);
+        else res.redirect('/list');
+    });
+});
+*/
+
+app.listen(port, function(){
+    console.log('server start :'+port);
+});
